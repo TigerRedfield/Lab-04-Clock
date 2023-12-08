fc80143bc642a88500443563b0a24fecd7b3053e
user <user@users-iMac.local>
2023-12-08 17:49:36 +0300

    Lab04-Clock

---
+++ Lab-04-Clock-Vasilev WatchKit Extension/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Lab-04-Clock-Vasilev WatchKit Extension
+//
+//  Created by user on 08.12.2023.
+//  Copyright © 2023 KITMobile. All rights reserved.
+//
+
+import Foundation
---
+++ Lab-04-Clock-Vasilev WatchKit Extension/TableRow.swift
@@ -0,0 +1,13 @@
+//
+//  TableRow.swift
+//  Lab-04-Clock-Vasilev WatchKit Extension
+//
+//  Created by user on 08.12.2023.
+//  Copyright © 2023 KITMobile. All rights reserved.
+//
+
+import WatchKit
+
+class TableRow: NSObject {
+
+}
